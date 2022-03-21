ed5489181243d8dd158332cb339d03c0cb9d4eb6
Peter Vantomme <r0740252@student.vives.be>
2022-03-21 12:06:34 +0100

    Added output test

--- .github/actions/unittest_output/run_unittest.py
+++ .github/actions/unittest_output/run_unittest.py
@@ -1,29 +1,19 @@
 # Simple unit test for checking that all wanted columns are in our raw data
 import pandas as pd
 
-TRAIN_COLUMNS = ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp' ,'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
-TEST_COLUMNS = ['PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+OUTPUT_COLUMNS = ["PassengerId","Survived"]
 
 def unittest_input_cols(df: pd.DataFrame, cols: str, dataset_name: str):
-    """Test whether columns in data match input columns
 
-    Args:
-        df (pd.DataFrame): train or test df
-        cols (str): columns to check against
-        dataset_name (str): name of dataset. Should be either 'train' or 'test'.
-    """ 
-    for col in df.columns:
-        assert col in cols, f'{col} missing in {dataset_name} dataset'
-    assert len(cols) == len(df.columns), f'More columns in {dataset_name} than expected'
+    for row in df.iterrows():
+        assert row[cols[1]] == 1 or 0, f"row with id {row[cols[0]]} has incorrect output format."
 
 def main():
     # Read data
-    train = pd.read_csv('data/raw/train.csv')
-    test = pd.read_csv('data/raw/test.csv')
+    output = pd.read_csv("data/output/final_predictions.csv")
     
     # Unit tests
-    unittest_input_cols(train, TRAIN_COLUMNS, 'train')
-    unittest_input_cols(test, TEST_COLUMNS, 'test')
+    unittest_input_cols(output, OUTPUT_COLUMNS, 'output')
     
     # Success
     print('All input unit tests passed!')
